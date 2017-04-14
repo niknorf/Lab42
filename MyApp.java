646d7bc44c95cfcd3cdb786fb866e485a210d9a9
niknorf <t5voni00@students.oamk.fi>
2017-04-14 19:32:43 +0300

    Update MyApp.java

--- MyApp.java
+++ MyApp.java
@@ -1,15 +1,22 @@
 import java.lang.Math; // headers MUST be above the first 
 
 public class main {
-
+  
   public static void main(String[] args) {
+     
      Product product = new Product("Edam", 3.3, 120);
-     product.changePrice(3.7);
+     
+     product.changeBalance(125);
+    
+     product.changePrice(3.4);
+
      System.out.println("Product value is " + product.countValue());
+
      product.printProduct();
    }
 }
 class Product {
+  
   private String name;
   private double price;
   private int amount; //Amount in storage
@@ -24,6 +31,10 @@ class Product {
     return(amount * price);
   }
   
+  public int changeBalance (int newbalance) {
+    this.amount = newbalance;
+    return amount;
+  }
   
   public double changePrice (double newprice) {
     this.price = newprice;
@@ -35,31 +46,4 @@ class Product {
     name,price,amount);
   }
   
-	 public static void main(String[] args) {
-		 Product product = new Product("Edam", 3.3, 120);
-		 product.changeBalance(125);
-		 System.out.println("Product value is " + product.countValue());
-		 product.printProduct();
-	 }
-	}
-class Product {
-		private String name;
-		private double price;
-		private int amount; //Amount in storage
-		public Product(String name, double price, int amount) {
-		this.name = name;
-		this.price = price;
-		this.amount = amount;
-		}
-		public double countValue() {
-		return(amount * price);
-		}
-		public void printProduct() {
-		System.out.printf("Product %s, price %4.1f and balance %d pcs", name,price,amount);
-		}
-
-		public int changeBalance (int newbalance) {
-	    this.amount = newbalance;
-	    return amount;
-	    }
 }
